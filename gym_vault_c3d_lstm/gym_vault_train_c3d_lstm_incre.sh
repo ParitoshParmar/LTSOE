91ad69576aeea3f3858e93fa0962d3ca577ff85e
ParitoshParmar <parmap1@unlv.nevada.edu>
2017-11-30 15:19:51 -0800

    Add files via upload

---
+++ gym_vault_c3d_lstm/gym_vault_train_c3d_lstm_incre.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+./build/tools/caffe \
+  train \
+  --solver=examples/gym_vault_c3d_lstm/solver_prototxts/c3d_lstm_incre_solver.prototxt \
+  --weights=/home/paritosh/Folder/vc/examples/c3d_ucf101/models_ucf101_gymvault/c3d_ucf101_iter_100000.caffemodel 2>&1 | tee -a examples/gym_vault_c3d_lstm/models_incre/gym_vault_c3d_lstm_incre.log
+
+  
+  
+  
+  
